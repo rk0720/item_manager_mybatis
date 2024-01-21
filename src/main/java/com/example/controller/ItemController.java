3775b84ca3d545e9b9f944a57bce4b0c4659836a
rk0720 <ryotakatsumataax@gmail.com>
2024-01-22 03:42:30 +0900

    kadai4 commit
    実装完了

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -6,19 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entity.Category;
 import com.example.entity.Item;
+import com.example.form.ItemForm;
+import com.example.service.CategoryService;
 import com.example.service.ItemService;
 
 @Controller
 @RequestMapping("/item")
 public class ItemController {
 	private final ItemService itemService;
+	private final CategoryService categoryService;
 	
 	@Autowired
-	public ItemController(ItemService itemService) {
+	public ItemController(ItemService itemService, CategoryService categoryService) {
 		this.itemService = itemService;
+		this.categoryService = categoryService;
 	}
 	
 	@GetMapping("index")
@@ -26,5 +32,12 @@ public class ItemController {
 		List<Item> items = this.itemService.findAll();
 		model.addAttribute("items", items);
 		return "index";
+	}
+	
+	@GetMapping("/create")
+	public String create(@ModelAttribute ItemForm itemForm, Model model) {
+		List<Category> categories = this.categoryService.findAll();
+		model.addAttribute("categories", categories);
+		return "create";
 	}
 }
